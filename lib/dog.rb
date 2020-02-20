02275addcd571bfe497d44c885137ed51456f529
Hunter Sides <hunter.s.1337@gmail.com>
2020-02-20 20:32:23 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,5 +1,21 @@
 class Dog 
-  def initialize(dog_name)
+  def initialize(dog_name,breed)
     @name = dog_name
   end
+  
+  def name=(dog_name)
+    @name = dog_name
+  end 
+  
+  def name 
+    @name 
+  end 
+  
+  def breed=(dog_name = "Mutt")
+    @breed = dog_name
+  end 
+  
+  def breed 
+    @breed 
+  end 
 end 
